9dc10bcd8d4b9e0632590b6941d8b7860838ca28
David Aspinall <David.Aspinall@ed.ac.uk>
2025-07-31 12:01:58 +0100

    Replace netstats -> whiff

--- scripts/BOT_calculations.sh
+++ scripts/BOT_calculations.sh
@@ -2,28 +2,28 @@
 
 # DDoS
 
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test CosineTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test PortTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test NearestNeighboursTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test CosineTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test PortTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DDoS --folder --csv data/BOT/  --test NearestNeighboursTest
 
 # DoS
 
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test CosineTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test PortTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test NearestNeighboursTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test CosineTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test PortTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target DoS --folder --csv data/BOT/  --test NearestNeighboursTest
 
 # Reconnaissance
 
-python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test CosineTest
-python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test PortTest
-python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test NearestNeighboursTest
+python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test CosineTest
+python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test PortTest
+python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Reconnaissance --folder --csv data/BOT/  --test NearestNeighboursTest
 
 # Theft
 
-python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test CosineTest
-python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test PortTest
-python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
-#python3 ./src/netstats.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test NearestNeighboursTest
+python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test CosineTest
+python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test PortTest
+python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test SingleFeatureEfficacyTest
+#python3 ./src/whiff.py --metadata metadata/bot/metadata.json --results results/BOT/ --target Theft --folder --csv data/BOT/  --test NearestNeighboursTest
--- scripts/CTU13_OTHER_calculations.sh
+++ scripts/CTU13_OTHER_calculations.sh
@@ -6,17 +6,17 @@
 
 # TCP ATTEMPT - Background
 
-python3 ./src/netstats.py --metadata metadata/ctu/donbot/6/TCP\ Attempt/metadata_bg.json --results results/CTU/6/background/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/6/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/donbot/6/TCP\ Attempt/metadata_bg.json --results results/CTU/6/background/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/6/  --test CompleteTest
 
 # TCP ATTEMPT - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/donbot/6/TCP\ Attempt/metadata_normal.json --results results/CTU/6/normal/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/6/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/donbot/6/TCP\ Attempt/metadata_normal.json --results results/CTU/6/normal/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/6/  --test CompleteTest
 
 ############################################################
 
 # TCP CC - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/donbot/6/CC/metadata_normal.json --results results/CTU/6/normal/ --target MAL_CC --folder --csv data/CTU/6/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/donbot/6/CC/metadata_normal.json --results results/CTU/6/normal/ --target MAL_CC --folder --csv data/CTU/6/  --test CompleteTest
 
 ###############################
 ######## Murlo 8 ##############
@@ -24,27 +24,27 @@ python3 ./src/netstats.py --metadata metadata/ctu/donbot/6/CC/metadata_normal.js
 
 # TCP ATTEMPT - Background
 
-python3 ./src/netstats.py --metadata metadata/ctu/murlo/8/TCP\ Attempt/metadata_bg.json --results results/CTU/8/background/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/8/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/murlo/8/TCP\ Attempt/metadata_bg.json --results results/CTU/8/background/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/8/  --test CompleteTest
 
 # TCP ATTEMPT - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/murlo/8/TCP\ Attempt/metadata_normal.json --results results/CTU/8/normal/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/8/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/murlo/8/TCP\ Attempt/metadata_normal.json --results results/CTU/8/normal/ --target MAL_TCP_ATTEMPT --folder --csv data/CTU/8/  --test CompleteTest
 
 ############################################################
 
 # TCP CC - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/murlo/8/CC/metadata_normal.json --results results/CTU/8/normal/ --target MAL_CC --folder --csv data/CTU/8/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/murlo/8/CC/metadata_normal.json --results results/CTU/8/normal/ --target MAL_CC --folder --csv data/CTU/8/  --test CompleteTest
 
 ############################################################
 
 # UDP Established - Background
 
-python3 ./src/netstats.py --metadata metadata/ctu/murlo/8/UDP\ Established/metadata_bg.json --results results/CTU/8/background/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/8/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/murlo/8/UDP\ Established/metadata_bg.json --results results/CTU/8/background/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/8/  --test CompleteTest
 
 # UDP Established - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/murlo/8/UDP\ Established/metadata_normal.json --results results/CTU/8/normal/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/8/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/murlo/8/UDP\ Established/metadata_normal.json --results results/CTU/8/normal/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/8/  --test CompleteTest
 
 ###############################
 ######## NSIS 12 ##############
@@ -52,28 +52,28 @@ python3 ./src/netstats.py --metadata metadata/ctu/murlo/8/UDP\ Established/metad
 
 # UDP ATTEMPT - Background
 
-python3 ./src/netstats.py --metadata metadata/ctu/nsis/12/UDP\ Attempt/metadata_bg.json --results results/CTU/12/background/ --target MAL_UDP_ATTEMPT --folder --csv data/CTU/12/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/nsis/12/UDP\ Attempt/metadata_bg.json --results results/CTU/12/background/ --target MAL_UDP_ATTEMPT --folder --csv data/CTU/12/  --test CompleteTest
 
 # UDP ATTEMPT - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/nsis/12/UDP\ Attempt/metadata_normal.json --results results/CTU/12/normal/ --target MAL_UDP_ATTEMPT --folder --csv data/CTU/12/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/nsis/12/UDP\ Attempt/metadata_normal.json --results results/CTU/12/normal/ --target MAL_UDP_ATTEMPT --folder --csv data/CTU/12/  --test CompleteTest
 
 ############################################################
 
 # UDP DNS - Background
 
-python3 ./src/netstats.py --metadata metadata/ctu/nsis/12/UDP\ DNS/metadata_bg.json --results results/CTU/12/background/ --target MAL_UDP_DNS --folder --csv data/CTU/12/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/nsis/12/UDP\ DNS/metadata_bg.json --results results/CTU/12/background/ --target MAL_UDP_DNS --folder --csv data/CTU/12/  --test CompleteTest
 
 # UDP DNS - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/nsis/12/UDP\ DNS/metadata_normal.json --results results/CTU/12/normal/ --target MAL_UDP_DNS --folder --csv data/CTU/12/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/nsis/12/UDP\ DNS/metadata_normal.json --results results/CTU/12/normal/ --target MAL_UDP_DNS --folder --csv data/CTU/12/  --test CompleteTest
 
 ############################################################
 
 # UDP Established - Background
 
-python3 ./src/netstats.py --metadata metadata/ctu/nsis/12/UDP\ Established/metadata_bg.json --results results/CTU/12/background/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/12/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/nsis/12/UDP\ Established/metadata_bg.json --results results/CTU/12/background/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/12/  --test CompleteTest
 
 # UDP Established - Normal
 
-python3 ./src/netstats.py --metadata metadata/ctu/nsis/12/UDP\ Established/metadata_normal.json --results results/CTU/12/normal/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/12/  --test CompleteTest
+python3 ./src/whiff.py --metadata metadata/ctu/nsis/12/UDP\ Established/metadata_normal.json --results results/CTU/12/normal/ --target MAL_UDP_ESTABLISHED --folder --csv data/CTU/12/  --test CompleteTest
